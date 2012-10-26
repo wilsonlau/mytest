7f68a609623cca46dd37c894425dc1078da31859
DevBox <wlau1@ubuntu.(none)>
2012-10-25 22:17:23 -0700

    added otheers

---
+++ app/models/Books.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author wlau1
+ */
+public class Books {
+    
+    public String testament;
+    public String book;
+    public String language;
+    
+}
